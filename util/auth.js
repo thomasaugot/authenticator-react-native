aaefc694c4dba4a63ae5653c999888edbe56b4aa
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-28 11:49:56 +0200

    sign up/ login logic done

--- util/auth.js
+++ util/auth.js
@@ -2,13 +2,19 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyC6gilHlw7uboIg8EZyE648Q8ayxRmJ-A4";
 
-async function createUser(email, password) {
-  const response = await axios.post(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=" + API_KEY,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+export async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+}
+
+export async function createUser(email, password) {
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
